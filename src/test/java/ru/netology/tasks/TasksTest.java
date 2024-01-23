726c9b36bb48c646dc07308cb4b63f40f8567c8f
Kate Bo <bodrakea@gmail.com>
2024-01-23 15:24:45 +0300

    init

---
+++ src/test/java/ru/netology/tasks/TasksTest.java
@@ -0,0 +1,80 @@
+package ru.netology.tasks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    @Test
+    public void shouldFindTaskInSimpleTask() {
+
+        Boolean expected = true;
+        Boolean actual = simpleTask.matches("Позвонить");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTaskInSimpleTask() {
+
+        Boolean expected = false;
+        Boolean actual = simpleTask.matches("hello");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInEpic() {
+
+        Boolean expected = true;
+        Boolean actual = epic.matches("Хле");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTaskInEpic() {
+
+        Boolean expected = false;
+        Boolean actual = epic.matches("Винишко");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInMeetingProject() {
+
+        Boolean expected = true;
+        Boolean actual = meeting.matches("НетоБанк");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTaskInMeetingTopic() {
+
+        Boolean expected = true;
+        Boolean actual = meeting.matches("Выкатка");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTaskInMeeting() {
+
+        Boolean expected = false;
+        Boolean actual = meeting.matches("СберБанк");
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/ru/netology/tasks/TodosTest.java
@@ -0,0 +1,60 @@
+package ru.netology.tasks;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+    String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    Todos todos = new Todos();
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTask() {
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("ни");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTask() {
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("отдых");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
